38915ed253ce10656915dd46742214ceb5d926c4
Gliktch <github@gliktch.com>
2026-01-29 18:30:27 +1000

    fix: keep spellbook menu open on cost failures

--- src/cmd5.c
+++ src/cmd5.c
@@ -72,6 +72,7 @@ static int get_spell(int *sn, cptr prompt, int sval, bool learned, int use_realm
     char        out_val[160];
     cptr        p;
     rect_t      display = ui_menu_rect();
+    rect_t      list_display = display;
     int menu_line = (use_menu ? 1 : 0);
 
 #ifdef ALLOW_REPEAT /* TNB */
@@ -140,6 +141,12 @@ static int get_spell(int *sn, cptr prompt, int sval, bool learned, int use_realm
     (void)strnfmt(out_val, 78, "(%^ss %c-%c, *=List, ESC=exit) %^s which %s? ",
         p, I2A(0), I2A(num - 1), prompt, p);
 
+    if (list_display.cy > 1)
+    {
+        list_display.y += 1;
+        list_display.cy -= 1;
+    }
+
     /* Get a spell from the user */
 
     choice = ESCAPE;
@@ -186,7 +193,7 @@ static int get_spell(int *sn, cptr prompt, int sval, bool learned, int use_realm
             }
             if (menu_line > num) menu_line -= num;
             /* Display a list of spells */
-            print_spells(menu_line, spells, num, display, use_realm);
+            print_spells(menu_line, spells, num, list_display, use_realm);
             if (ask) continue;
         }
         else
@@ -204,7 +211,7 @@ static int get_spell(int *sn, cptr prompt, int sval, bool learned, int use_realm
                     screen_save();
 
                     /* Display a list of spells */
-                    print_spells(menu_line, spells, num, display, use_realm);
+                    print_spells(menu_line, spells, num, list_display, use_realm);
                 }
 
                 /* Hide the list */
@@ -252,21 +259,46 @@ static int get_spell(int *sn, cptr prompt, int sval, bool learned, int use_realm
             continue;
         }
 
+        if (!is_magic(use_realm))
+        {
+            s_ptr = &technic_info[use_realm - MIN_TECHNIC][spell];
+        }
+        else
+        {
+            s_ptr = &mp_ptr->info[use_realm - 1][spell];
+        }
+
+        if (learned && !browse)
+        {
+            bool hp_caster = (caster_ptr && (caster_ptr->options & CASTER_USE_HP));
+            if (p_ptr->pclass == CLASS_NINJA_LAWYER && use_realm != REALM_LAW)
+                hp_caster = TRUE;
+
+            if (use_realm == REALM_HISSATSU)
+                need_mana = s_ptr->smana;
+            else
+                need_mana = mod_need_mana(lawyer_hack(s_ptr, LAWYER_HACK_MANA), spell, use_realm);
+
+            if (hp_caster && need_mana > p_ptr->chp)
+            {
+                msg_print("You do not have enough hit points to use this spell.");
+                if (flush_failure) flush();
+                continue;
+            }
+            if (!hp_caster && need_mana > p_ptr->csp)
+            {
+                msg_format("You do not have enough mana to %s this %s.", prompt, p);
+                if (flush_failure) flush();
+                continue;
+            }
+        }
+
         /* Verify it */
         if (ask)
         {
             char tmp_val[160];
 
             /* Access the spell */
-            if (!is_magic(use_realm))
-            {
-                s_ptr = &technic_info[use_realm - MIN_TECHNIC][spell];
-            }
-            else
-            {
-                s_ptr = &mp_ptr->info[use_realm - 1][spell];
-            }
-
             /* Extract mana consumption rate */
             if (use_realm == REALM_HISSATSU)
             {
